ae01577d0480f49fdac296e3a8c425ed3cc2b82d
kkssii0210 <kkssii0210@naver.com>
2023-11-09 15:29:43 +0900

    각 행 클릭시 게시글 보기 링크로 이동

--- src/page/BoardList.js
+++ src/page/BoardList.js
@@ -10,9 +10,11 @@ import {
   Tr,
 } from "@chakra-ui/react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export function BoardList() {
   const [boardList, setBoardList] = useState([]);
+  const navigate = useNavigate();
   useEffect(() => {
     axios
       .get("/api/board/list")
@@ -37,7 +39,11 @@ export function BoardList() {
               <Spinner />
             ) : (
               boardList.map((board) => (
-                <Tr key={board.id}>
+                <Tr
+                  _hover={{ cursor: "pointer" }}
+                  key={board.id}
+                  onClick={() => navigate("/board/" + board.id)}
+                >
                   <Td>{board.id}</Td>
                   <Td>{board.title}</Td>
                   <Td>{board.writer}</Td>
