814684c02b9b3148c7200969dd8a2006ef9817f6
kkssii0210 <kkssii0210@naver.com>
2023-11-09 16:22:41 +0900

    게시물 보기 화면 정리

--- src/page/BoardView.js
+++ src/page/BoardView.js
@@ -1,13 +1,22 @@
 import { useParams } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Box, Spinner } from "@chakra-ui/react";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Spinner,
+  Textarea,
+} from "@chakra-ui/react";
 
 export function BoardView() {
   const { id } = useParams();
   const [board, setBoard] = useState(null);
   useEffect(() => {
-    axios.get("/api/board/id/" + id).then((response) => setBoard(board));
+    axios
+      .get("/api/board/id/" + id)
+      .then((response) => setBoard(response.data));
   }, []);
   if (board === null) {
     return <Spinner />;
@@ -15,11 +24,22 @@ export function BoardView() {
   return (
     <Box>
       <h1>글 보기</h1>
-      <p>번호: {board.id}</p>
-      <p>제목: {board.title}</p>
-      <p>본문: {board.content}</p>
-      <p>작성자: {board.writer}</p>
-      <p>작성일시: {board.inserted}</p>
+      <FormControl>
+        <FormLabel>제목</FormLabel>
+        <Input value={board.title} readOnly />
+      </FormControl>
+      <FormControl>
+        <FormLabel>본문</FormLabel>
+        <Textarea value={board.content} readOnly />
+      </FormControl>
+      <FormControl>
+        <FormLabel>작성자</FormLabel>
+        <Input value={board.writer} readOnly />
+      </FormControl>
+      <FormControl>
+        <FormLabel>작성일시</FormLabel>
+        <Input value={board.inserted} readOnly />
+      </FormControl>
     </Box>
   );
 }
