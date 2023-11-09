c490fa1135d3231e16ddf33cb7ec81a1cf4b7ca5
kkssii0210 <kkssii0210@naver.com>
2023-11-09 14:28:17 +0900

    저장시 저장버튼 비활성화

--- src/page/BoardWrite.js
+++ src/page/BoardWrite.js
@@ -14,10 +14,11 @@ export function BoardWrite() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [writer, setWriter] = useState("");
-
   const toast = useToast();
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   function handleSubmit() {
+    setIsSubmitting(true);
     axios
       .post("/api/board/add", {
         title,
@@ -30,13 +31,21 @@ export function BoardWrite() {
           status: "success",
         });
       })
-      .catch(() => {
-        toast({
-          description: "저장 중에 문제가 발생하였습니다.",
-          status: "error",
-        });
+      .catch((error) => {
+        console.log(error.response.status);
+        if (error.response.status === 400) {
+          toast({
+            description: "작성한 내용을 확인해주세요.",
+            status: "error",
+          });
+        } else {
+          toast({
+            description: "저장 중에 문제가 발생하였습니다.",
+            status: "error",
+          });
+        }
       })
-      .finally(() => console.log("끝"));
+      .finally(() => setIsSubmitting(false));
   }
 
   return (
@@ -61,7 +70,11 @@ export function BoardWrite() {
             onChange={(e) => setWriter(e.target.value)}
           ></Input>
         </FormControl>
-        <Button onClick={handleSubmit} colorScheme="blue">
+        <Button
+          isDisabled={isSubmitting}
+          onClick={handleSubmit}
+          colorScheme="blue"
+        >
           저장
         </Button>
       </Box>
