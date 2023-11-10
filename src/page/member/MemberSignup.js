9aee2551b1403a823b1de6adcab3c9ef08e33101
kkssii0210 <kkssii0210@naver.com>
2023-11-10 15:21:20 +0900

    이메일중복검증 추가

--- src/page/member/MemberSignup.js
+++ src/page/member/MemberSignup.js
@@ -1,10 +1,12 @@
 import {
   Box,
   Button,
+  Flex,
   FormControl,
   FormErrorMessage,
   FormLabel,
   Input,
+  useToast,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import axios from "axios";
@@ -14,13 +16,28 @@ export function MemberSignup() {
   const [password, setPassword] = useState("");
   const [passwordCheck, setPasswordCheck] = useState("");
   const [email, setEmail] = useState("");
+  const [emailAvailable, setEmailAvailable] = useState(false);
+  const [idAvailable, setIdAvailable] = useState(false);
+  const toast = useToast();
+
   let submitAvailable = true;
+
+  if (!emailAvailable) {
+    submitAvailable = false;
+  }
+
+  if (!idAvailable) {
+    submitAvailable = false;
+  }
+
   if (password != passwordCheck) {
     submitAvailable = false;
   }
+
   if (password.length === 0) {
     submitAvailable = false;
   }
+
   function handleSubmit() {
     axios
       .post("/api/member/signup", {
@@ -33,12 +50,68 @@ export function MemberSignup() {
       .finally(() => console.log("done"));
   }
 
+  function handleIdCheck() {
+    const searchParam = new URLSearchParams();
+    searchParam.set("id", id);
+    axios
+      .get("/api/member/check?" + searchParam.toString())
+      .then(() => {
+        setIdAvailable(false);
+        toast({
+          description: "이미 사용중인 아이디입니다",
+          status: "warning",
+        });
+      })
+      .catch((error) => {
+        if (error.response.status === 404) {
+          setIdAvailable(true);
+          toast({
+            description: "사용 가능한 아이디입니다.",
+            status: "success",
+          });
+        }
+      });
+  }
+
+  function handleEmailCheck() {
+    const params = new URLSearchParams();
+    params.set("email", email);
+    axios
+      .get("/api/member/check?" + params)
+      .then(() => {
+        setEmailAvailable(false);
+        toast({
+          description: "이미 사용중인 email입니다",
+          status: "warning",
+        });
+      })
+      .catch((error) => {
+        if (error.response.status === 404) {
+          setEmailAvailable(true);
+          toast({
+            description: "사용 가능한 email입니다.",
+            status: "success",
+          });
+        }
+      });
+  }
+
   return (
     <Box>
       <h1>회원 가입</h1>
-      <FormControl>
+      <FormControl isInvalid={!idAvailable}>
         <FormLabel>id</FormLabel>
-        <Input value={id} onChange={(e) => setId(e.target.value)} />
+        <Flex>
+          <Input
+            value={id}
+            onChange={(e) => {
+              setId(e.target.value);
+              setIdAvailable(false);
+            }}
+          />
+          <Button onClick={handleIdCheck}>중복확인</Button>
+        </Flex>
+        <FormErrorMessage>ID 중복체크를 해주세요.</FormErrorMessage>
       </FormControl>
       <FormControl isInvalid={password.length === 0}>
         <FormLabel>password</FormLabel>
@@ -47,7 +120,8 @@ export function MemberSignup() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <FormErrorMessage>암호를 입력해 주세요</FormErrorMessage>
+
+        <FormErrorMessage>암호를 입력해 주세요.</FormErrorMessage>
       </FormControl>
       <FormControl isInvalid={password != passwordCheck}>
         <FormLabel>password 확인</FormLabel>
@@ -56,15 +130,22 @@ export function MemberSignup() {
           value={passwordCheck}
           onChange={(e) => setPasswordCheck(e.target.value)}
         />
-        <FormErrorMessage>암호가 다릅니다</FormErrorMessage>
+        <FormErrorMessage>암호가 다릅니다.</FormErrorMessage>
       </FormControl>
-      <FormControl>
+      <FormControl isInvalid={!emailAvailable}>
         <FormLabel>email</FormLabel>
-        <Input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+        <Flex>
+          <Input
+            type="email"
+            value={email}
+            onChange={(e) => {
+              setEmailAvailable(false);
+              setEmail(e.target.value);
+            }}
+          />
+          <Button onClick={handleEmailCheck}>중복체크</Button>
+        </Flex>
+        <FormErrorMessage>email 중복 체크를 해주세요</FormErrorMessage>
       </FormControl>
       <Button
         isDisabled={!submitAvailable}
