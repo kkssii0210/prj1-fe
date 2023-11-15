48e602d8c6a71531185615ce73521a63f5017ccb
kkssii0210 <kkssii0210@naver.com>
2023-11-15 15:43:18 +0900

    댓글 삭제기능 완료

--- src/component/CommentContainer.js
+++ src/component/CommentContainer.js
@@ -10,9 +10,20 @@ import {
   Textarea,
   Text,
   Flex,
+  useDisclosure,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  Modal,
+  useToast,
 } from "@chakra-ui/react";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import { DeleteIcon } from "@chakra-ui/icons";
+import { LoginContext } from "./LoginProvider";
 
 function CommentForm({ boardId, isSubmitting, onSubmit }) {
   const [comment, setComment] = useState("");
@@ -29,7 +40,8 @@ function CommentForm({ boardId, isSubmitting, onSubmit }) {
   );
 }
 
-function CommentList({ commentList }) {
+function CommentList({ commentList, onDeleteModalOpen, isSubmitting }) {
+  const { isAuthenticated, hasAccess } = useContext(LoginContext);
   return (
     <Card>
       <CardHeader>
@@ -42,6 +54,16 @@ function CommentList({ commentList }) {
               <Flex justifyContent="space-between">
                 <Heading size="xs">{comment.memberId}</Heading>
                 <Text fontSize="xs">{comment.inserted}</Text>
+
+                {hasAccess(comment.memberId) && (
+                  <Button
+                    isDisabled={isSubmitting}
+                    onClick={() => onDeleteModalOpen(comment.id)}
+                    colorScheme="red"
+                  >
+                    <DeleteIcon />
+                  </Button>
+                )}
               </Flex>
               <Text sx={{ whiteSpace: "pre-wrap" }} pt="2" fontSize="sm">
                 {comment.comment}
@@ -56,8 +78,11 @@ function CommentList({ commentList }) {
 
 export function CommentContainer({ boardId }) {
   const [isSubmitting, setIsSubmitting] = useState(false);
-
   const [commentList, setCommentList] = useState([]);
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  const commentIdRef = useRef(0);
+  const { isAuthenticated } = useContext(LoginContext);
+  const toast = useToast();
   useEffect(() => {
     if (!isSubmitting) {
       const params = new URLSearchParams();
@@ -71,16 +96,95 @@ export function CommentContainer({ boardId }) {
     setIsSubmitting(true);
     axios
       .post("/api/comment/add", comment)
-      .finally(() => setIsSubmitting(false));
+      .then(() => {
+        toast({
+          description: "댓글이 등록됐습니다.",
+          status: "success",
+        });
+      })
+      .catch((error) => {
+        if (error.res.status === 401 || error.response.status === 403) {
+          toast({
+            description: "권한이 없습니다.",
+            status: "warning",
+          });
+        } else {
+          toast({
+            description: "댓글 삭제중 문제발생.",
+            status: "error",
+          });
+        }
+      })
+      .finally(() => {
+        setIsSubmitting(false);
+      });
+  }
+  function handleCommentDelete() {
+    setIsSubmitting(true);
+    axios
+      .delete("/api/comment/delete/" + commentIdRef.current)
+      .then(() => {
+        setCommentList(
+          commentList.filter((comment) => comment.id !== commentIdRef.current),
+        );
+        toast({
+          description: "댓글이 삭제됐습니다.",
+          status: "success",
+        });
+      })
+      .catch((error) => {
+        if (error.res.status === 401 || error.response.status === 403) {
+          toast({
+            description: "권한이 없습니다.",
+            status: "warning",
+          });
+        } else {
+          toast({
+            description: "댓글 삭제중 문제발생.",
+            status: "error",
+          });
+        }
+      })
+      .finally(() => {
+        setIsSubmitting(false);
+        onClose();
+      });
+  }
+
+  function handleDeleteModalOpen(commentId) {
+    commentIdRef.current = commentId;
+    onOpen();
   }
   return (
     <Box>
-      <CommentForm
+      {isAuthenticated() && (
+        <CommentForm
+          boardId={boardId}
+          isSubmitting={isSubmitting}
+          onSubmit={handleSubmit}
+        />
+      )}
+      <CommentList
         boardId={boardId}
         isSubmitting={isSubmitting}
-        onSubmit={handleSubmit}
+        commentList={commentList}
+        onDeleteModalOpen={handleDeleteModalOpen}
       />
-      <CommentList boardId={boardId} commentList={commentList} />
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>삭제 확인</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>삭제 하시겠습니까?</ModalBody>
+
+          <ModalFooter>
+            <Button onClick={onClose}>닫기</Button>
+            <Button onClick={handleCommentDelete} colorScheme="red">
+              삭제
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
